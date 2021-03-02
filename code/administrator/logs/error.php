d00dab4b5b724672967ef30252a364e0a7f7bfc2
warangkanakh <warangkana.khem@gmail.com>
2021-03-03 00:07:44 +0700

    add logo and edit announcement page

---
+++ code/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-03-02 02:39:53 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-03-02T02:39:53+00:00	INFO 172.18.0.1	joomlafailure	ชื่อผู้ใช้และรหัสผ่านไม่ตรงกันหรือคุณยังไม่มีบัญชี
